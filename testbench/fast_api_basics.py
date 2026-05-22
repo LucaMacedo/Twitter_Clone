1ba95e8483fdc882f0e129ae60ff0cebd49e54e1
Gian-Luca Mocerino Macedo <gian-luca.mocerinomacedo@hs-weingarten.de>
2026-05-22 13:45:02 +0200

    GET and POST for products

--- testbench/fast_api_basics.py
+++ testbench/fast_api_basics.py
@@ -2,13 +2,28 @@ from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
-
+products = []
 # decorator to define a GET endpoint.
 # Test the application at http://127.0.0.1:4444/docs
-@app.get("/") # root point
-async def index(): # no asyncio is needed, but it is a good practice to use async def for better performance
-    return {"message": "hello world"}
+@app.get("/")
+async def get_products(): # no asyncio is needed, but it is a good practice to use async def for better performance
+    return products
 
-if __name__ == "__main__": # start application when it's main module. Otherwise you have to use the terminal to start the app.
-    uvicorn.run(app, host="127.0.0.1", port=4444)
+@app.get("/products/{product_id}")
+async def get_product(product_id: int):
+    for product in products:
+        if product.get("id") == product_id:
+            return product
+    return {"error": "Produkt nicht gefunden"}
 
+@app.post("/products")
+async def create_product(product: dict):
+    products.append(product)
+    return {"success": "Produkt erstellt"}
+
+
+
+# if __name__ == "__main__": # start application when it's main module. Otherwise you have to use the terminal to start the app.
+#     uvicorn.run(app, host="127.0.0.1", port=4444)
+
+# run in shell uvicorn fast_api_basics:app --port 4444
