d8e779b54805283c8772b0c997a8ff5976422fd7
Gian-Luca Mocerino Macedo <gian-luca.mocerinomacedo@hs-weingarten.de>
2026-05-22 14:04:13 +0200

    POST and DELETE added

--- testbench/fast_api_basics.py
+++ testbench/fast_api_basics.py
@@ -16,6 +16,22 @@ async def get_product(product_id: int):
             return product
     return {"error": "Produkt nicht gefunden"}
 
+@app.put("/products/{product_id}")
+async def update_product(product_id: int, product: dict):
+    for index, p in enumerate(products): # use index to with product
+        if p.get("id") == product_id:
+            products[index] = product # update with new product when condition is fullfilled
+            return {"success": "Produkt geupdated"}
+    return {"error": "Produkt nicht gefunden"} # temporary solution for error handling. In production you should use proper error handling with HTTPException and status codes.
+
+@app.delete("/products/{product_id}")
+async def delete_product(product_id: int):
+    for index, product in enumerate(products):
+        if product.get("id") == product_id:
+            products.pop(index) # delete product when condition is fullfilled
+            return {"success": "Produkt gelöscht"}
+    return {"error": "Produkt nicht gefunden"}
+
 @app.post("/products")
 async def create_product(product: dict):
     products.append(product)
