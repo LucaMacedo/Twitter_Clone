fd6c11e447d4cf2634dbe1bef34bffc02e9b4a7c
Gian-Luca Mocerino Macedo <gian-luca.mocerinomacedo@hs-weingarten.de>
2026-05-25 10:34:35 +0200

    SQlAlchemy with FastAPI

--- testbench/db_basics.py
+++ testbench/db_basics.py
@@ -2,9 +2,13 @@ from sqlalchemy.ext.declarative import declarative_base # Funkiton für Klasse, 
 from sqlalchemy import Column, Integer, String, Float, create_engine # Spalten für die Tabelle und Datentypen. Engine für DB
 from sqlalchemy.orm import sessionmaker # Session für DB-Operationen
 # A database engine (or storage engine) is the underlying software component that a database management system (DBMS) uses to create, read, update and delete (CRUD) data from a database.
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr # EmailStr ist ein spezieller Datentyp, der von Pydantic bereitgestellt wird und zur Validierung von E-Mail-Adressen verwendet wird. Er stellt sicher, dass die eingegebene E-Mail-Adresse ein gültiges Format hat.
 
 engine = create_engine("sqlite:///users.db")
 Base = declarative_base() # Base-Klasse für die Deklaration der Tabellen
+
+app = FastAPI()
 
 class User(Base): # User-Klasse, die von Base erbt. Sie repräsentiert die Tabelle "users" in der Datenbank.
     __tablename__ = "users" 
@@ -16,15 +20,32 @@ class User(Base): # User-Klasse, die von Base erbt. Sie repräsentiert die Tabel
     password = Column(String)
     age = Column(Integer)
 
+class UserModel(BaseModel):
+    # sqlchemy legt id selbst an
+    firstname: str
+    lastname: str
+    email: EmailStr
+    password: str
+    age: int
+
 Base.metadata.create_all(engine) # erstellt die Tabelle in der Datenbank, wenn sie noch nicht existiert   
 Session = sessionmaker(bind=engine) # Session-Klasse, die an die Engine gebunden ist. Sie ermöglicht es uns, Transaktionen mit der Datenbank durchzuführen.
 session = Session()
-new_user = User(firstname="John", lastname="Doe", email="johndeo@example.com", password="123456", age=30)
-session.add(new_user)
-session.commit()
+# Ersetzt durch Funktion
+# new_user = User(firstname="John", lastname="Doe", email="johndeo@example.com", password="123456", age=30)
+# session.add(new_user)
+# session.commit()
 
-# user aus db. Übergib Objekte
-all_users = session.query(User).all()
+# # user aus db. Übergib Objekte
+# all_users = session.query(User).all()
 
-for user in all_users:
-    print(user.id, user.firstname, user.lastname)
+# for user in all_users:
+#     print(user.id, user.firstname, user.lastname)
+
+@app.post("/register/", status_code=201)
+def create_user(user: UserModel):
+    new_user = User(**user.dict()) # dict() konvertiert das Pydantic-Modell in ein Dictionary, das dann an den Konstruktor der User-Klasse übergeben wird.
+    #** entpackt dict. Bsp: name: "John", usw. Dict wird nicht komplett übergeben
+    session.add(new_user)
+    session.commit()
+    return {"id": new_user.id, "message": "User erfolgreich registriert"}
