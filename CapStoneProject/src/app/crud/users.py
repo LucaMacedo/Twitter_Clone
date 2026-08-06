9b210b301f38a5d9835ac4d3d7f801f8c5327f61
Luca Macedo <luca.macedo39@gmx.de>
2026-08-06 18:54:07 +0200

    login endpoint

--- CapStoneProject/src/app/crud/users.py
+++ CapStoneProject/src/app/crud/users.py
@@ -1,6 +1,7 @@
 from app.db_and_models.models import UserModel, User
 from fastapi import HTTPException
 from sqlmodel import Session, select
+from fastapi.security import OAuth2PasswordRequestForm
 
 async def create_user(usermodel: UserModel, db: Session):
 
@@ -13,3 +14,9 @@ async def create_user(usermodel: UserModel, db: Session):
     db.commit()
     db.refresh(user)
     return {"success": f"User mit {user.id} erstellt"}
+
+async def login_user(form_data: OAuth2PasswordRequestForm, db: Session):
+    existing_user = db.exec(select(User).where(User.username == form_data.username)).first()
+    if not existing_user:
+        raise HTTPException(status_code=401, detail="Not able to be authenticated")
+    return {"access_token": "faketoken", "token_type": "bearer"}
--- CapStoneProject/src/app/routers/users.py
+++ CapStoneProject/src/app/routers/users.py
@@ -2,10 +2,15 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.db_and_models.models import UserModel, User
 from sqlmodel import Session
 from app.db_and_models.session import get_session
-from app.crud.users import create_user
+from app.crud.users import create_user, login_user
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=["Users"])
 
 @router.post("/users")
 async def create_user_endpoint(usermodel: UserModel, db: Session = Depends(get_session)):
     return await create_user(usermodel=usermodel, db=db)
+
+@router.post("/login")
+async def login_user_endpoint(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_session)):
+    return await login_user(form_data=form_data, db=db)
